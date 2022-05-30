36044254d7f04d3566a46cb220bface1675209fa
Farhan <mdfarhansamsuri@gmail.com>
2022-05-30 15:23:53 +0800

    hello world

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MainContainer from './screen/MainContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainContainer />
   </React.StrictMode>
 );
 
---
+++ src/screen/MainContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const MainContainer = () => {
+
+    return (
+
+        <>
+        Hello World
+        </>
+    );
+}
+
+export default MainContainer;
